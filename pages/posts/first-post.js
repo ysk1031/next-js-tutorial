0185feef9a8bbc7308f56e65106afab74bf936f7
Yusuke Aono <yusukeaono.1031@gmail.com>
2021-02-19 13:29:37 +0900

    Refactor

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../components/layout"
 
-export default function FirstPost() {
+const FirstPost = () => {
   return (
     <Layout>
       <Head>
@@ -15,5 +15,6 @@ export default function FirstPost() {
         </Link>
       </h2>
     </Layout>
-  )
+  );
 }
+export default FirstPost;
