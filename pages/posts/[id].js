0dd680a73c0f05cbb8a90b02f0f6f6756fddb48c
Yusuke Aono <yusukeaono.1031@gmail.com>
2021-02-26 00:21:36 +0900

    Add date formatter

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import { Layout } from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from "next/head";
+import { Date } from "../../components/date";
 
 export const getStaticPaths = async () => {
   const paths = getAllPostIds();
@@ -30,7 +31,7 @@ const Post = ({ postData }) => {
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
