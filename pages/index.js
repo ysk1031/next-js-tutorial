86fa5cb95380064cd476a5a7f25e6626bb14c0a9
Yusuke Aono <yusukeaono.1031@gmail.com>
2021-02-26 00:34:03 +0900

    Add link to top page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import { Layout, siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from '../lib/posts';
+import Link from "next/link";
+import { Date } from "../components/date";
 
 export const getStaticProps = async () => {
   const allPostsData = getSortedPostsData();
@@ -32,11 +34,13 @@ const Home = ({ allPostsData }) => {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title}) => (
             <li className={utilStyles.listItem} key={{id}}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
