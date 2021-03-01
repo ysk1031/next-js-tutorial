e6a5fb8ee6ca1a471082266135bb619ca4543590
Yusuke Aono <yusukeaono.1031@gmail.com>
2021-03-01 21:42:33 +0900

    Prettier

--- src/components/date.tsx
+++ src/components/date.tsx
@@ -1,7 +1,11 @@
-import { parseISO, format } from 'date-fns';
+import { parseISO, format } from "date-fns";
 import React from "react";
 
-export const Date: React.FC<{dateString: string}> = ({ dateString }: { dateString: string }) => {
+export const Date: React.FC<{ dateString: string }> = ({
+  dateString,
+}: {
+  dateString: string;
+}) => {
   const date: Date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
 };
